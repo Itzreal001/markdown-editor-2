0dab0600719aacc9754968f736ba72b23aea88c8
Itzreal001 <israellawal323@gmail.com>
2025-07-04 11:14:31 +0100

    UI redesign v2

---
+++ OneDrive/Desktop/projects/markdown editor/webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Markdown Editor'
+    })
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 3000
+  },
+  mode: 'development'
+};
